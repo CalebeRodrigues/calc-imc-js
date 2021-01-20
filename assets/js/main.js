466a6843278829c94778510d545478e77b32ffbf
Calebe Rodrigues <calebebrodrigues@gmail.com>
2021-01-20 13:07:30 -0300

    JavaScript implemented

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,26 @@
 main();
 
 function main (){
-    alert('Testando!');
+    
+    const form = document.querySelector(".form");
+
+    const resultado = document.querySelector("#resultado");
+
+    form.addEventListener("submit", calculaIMC );
+
+
+    function calculaIMC (evento){
+
+        evento.preventDefault();
+    
+        const peso = Number(form.querySelector("#peso").value);
+    
+        const altura = Number(form.querySelector("#altura").value);
+    
+        const calc = peso / altura**2;
+    
+        console.log('Deu certo.');
+        console.log("Resultado = " + calc.toFixed(2));
+    }
 }
 
